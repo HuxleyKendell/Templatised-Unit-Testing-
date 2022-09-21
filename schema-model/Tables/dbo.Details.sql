c97e4386c1218a8261d004bfff8c4bd6da84a22d
Huxley Kendell <Huxley.Kendell@red-gate.com>
2022-09-21 15:01:18 +0100

    Committing Intial Schema Model

---
+++ schema-model/Tables/dbo.Details.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[Details]
+(
+[EmployeeID] [nchar] (10) NULL,
+[FirstName] [nchar] (15) NULL,
+[LastName] [nchar] (15) NULL,
+[Address] [nvarchar] (max) NULL,
+[DOB] [nchar] (10) NULL
+)
+GO

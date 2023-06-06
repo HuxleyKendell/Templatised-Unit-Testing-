f70629bca2dce795e96be7615c33a1803750fb9e
HuxleyKendell <Huxley.Kendell@red-gate.com>
2023-06-06 11:18:57 +0100

    Added Pipeline From ADO

--- schema-model/Tables/dbo.Details.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[Details]
-(
-[EmployeeID] [nchar] (10) NULL,
-[FirstName] [nchar] (15) NULL,
-[LastName] [nchar] (15) NULL,
-[Address] [nvarchar] (max) NULL,
-[DOB] [nchar] (10) NULL
-)
-GO
